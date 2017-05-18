eb85878c1e3e6d0de169e84b4cbb206b875f92b3
Aokijikuzan <florenzabs@hotmail.fr>
2017-05-18 17:39:42 +0200

    5em commit

--- src/MultiServiceGsm/FrontBundle/Controller/ApplController.php
+++ src/MultiServiceGsm/FrontBundle/Controller/ApplController.php
@@ -1,21 +1,22 @@
 <?php
 
 namespace MultiServiceGsm\FrontBundle\Controller;
-
+use MultiServiceGsm\FrontBundle\Entity\Modele;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class ApplController extends Controller
 {
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-
+/*
         $marques = $em->getRepository('MultiServiceGsmFrontBundle:Marque')->findAll(); 
-        
         return $this->render('MultiServiceGsmFrontBundle:Default:index.html.twig', array( 'marques' =>$marques ));
+*/
+        $modeles = $em->getRepository('MultiServiceGsmFrontBundle:Modele')->findAll();
+        return $this->render('MultiServiceGsmFrontBundle:Marque:Apple.html.twig', array(
+            'modeles' => $modeles
+        ));
     }
-    public function infAction()
-    {
-		return $this->render('MultiServiceGsmFrontBundle:Default:pageInfo.html.twig');
-	}
+   
 }
