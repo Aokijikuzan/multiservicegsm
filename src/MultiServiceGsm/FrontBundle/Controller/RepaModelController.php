1fbb3a8274b77d92ffe21c68e1ba61d263c89719
Aokijikuzan <florenzabs@hotmail.fr>
2017-06-01 20:56:51 +0200

    commit le 1/06/2017 à 20h56

--- src/MultiServiceGsm/FrontBundle/Controller/RepaModelController.php
+++ src/MultiServiceGsm/FrontBundle/Controller/RepaModelController.php
@@ -14,10 +14,14 @@ class RepaModelController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $modeles = $em->getRepository('MultiServiceGsmFrontBundle:Modele')->findByMarque($marque);
+
+ 		$reparations = $em->getRepository('MultiServiceGsmFrontBundle:Reparation')->findAll();
+        
         $tarif = $em->getRepository('MultiServiceGsmFrontBundle:Tarif')->findByModel($modele);
         //return $this->render('MultiServiceGsmFrontBundle:Marque:Apple.html.twig', array(
        //     'modeles' => $modeles
        return $this-> render('MultiServiceGsmFrontBundle:Modele:affichReparations.html.twig',array('modeles' => $modeles,'tarif'=>$tarif,$marque));
+
     }
    
 }
